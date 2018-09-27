07f15eb19711de4f065f80b4e8998562c23667e8
dhinske <david.hinske@gmx.net>
2018-09-27 13:46:50 +0200

    feature: folder and multibranch support

--- jenkins/script-console/deleteOldBuilds.groovy
+++ jenkins/script-console/deleteOldBuilds.groovy
@@ -2,14 +2,24 @@
 Deletes all builds for all jobs except the last {keepAmount}
 **/
 
-def keepAmount = 8	// number of builds to keep for each job
+def keepAmount = 10	// number of builds to keep for each job
 
-Jenkins.instance.getAllItems().findAll { job ->
-  def latest = job.builds.getLastBuild().number
-  println latest
+Jenkins.instance.getAllItems().findAll { item ->
+  if (!(item instanceof com.cloudbees.hudson.plugins.folder.Folder) && !(item instanceof org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject)) {
+    deleteOldBuilds(item, keepAmount)   
+  }
+}
+
+def deleteOldBuilds(job, keepAmount) {
+  def latest = 0
+  if (job.builds.getLastBuild() != null) {
+    latest = job.builds.getLastBuild().number
+  }
+  println job.name + " " + latest
   job.builds.findAll { build ->
     build.number <= (latest - keepAmount)
   }.each {
-    build.delete()
+    it.delete()
+    print "|"
   } 
 }
